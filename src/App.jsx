05006589d5dc7f5458809acfb8684679885dd65f
obaydullaa <cr7obaydul@gmail.com>
2024-11-30 23:17:31 +0600

    add react componet quizze.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import {useState} from  'react';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const [quizzes, setQuizzes] = useState(0)
+  const fetchQuiz =  async() => {
+    const res = await fetch('https://opentdb.com/api.php?amount=5&category=18&difficulty=easy&type=multiple');
+    const {results} = res.json();
+    setQuizzes(results)
+    console.log(results)
+  }
+    return (
+        <div className="container sm mx-auto placeholder:bg-fuchsia-100 ">
+            <h1 className="text-3xl font-bold">Hello from React</h1>
+            <button className="mt-5 py-2 px-5 bg-violet-500 text-white font-semibold rounded-full shadow-md hover:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-400 focus:ring-opacity-75" onClick={fetchQuiz}>Star Quiz</button>
+        </div>
+    );
 }
 
-export default App
+export default App;
